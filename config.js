29d893ade2b552981dbc206a05437f58a2bac192
Aditya Ranjan Swain <85053443+AdityaLiza@users.noreply.github.com>
2025-06-07 08:10:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
+global.Owner = ['919436984401']
+global.OwnerNumber = ['919436984401']
+global.ownertag = ['919436984401']
+global.BotName = "Lizu"
 global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.author = "By: Aditya"
+global.OwnerName = "Aditya"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 // Prefix //
 // If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
-global.prefa = ['-'] 
+global.prefa = ['!'] 
 
 
 global.location = "West Bengal, India"
